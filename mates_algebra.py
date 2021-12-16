d270191b859690d66be95429175d73e300dcb3b2
clblommberg <clblommberg@gmail.com>
2021-12-15 23:53:28 -0500

    comind

---
+++ mates_algebra.py
@@ -0,0 +1,14 @@
+import sympy 
+x = sympy.symbols('x') 
+p = sympy.Poly(x**2) 
+p
+q = sympy.Poly(1+x+x**3) 
+q
+
+
+print("Hola mundo")
+
+print("Hola mundo1")
+print("Hola mundo2")
+print("Hola mundo3")
+print("Hola mundo4")
